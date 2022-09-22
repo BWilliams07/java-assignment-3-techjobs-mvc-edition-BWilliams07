0dde693c604ae0afe392b087d85426ae0c81d79c
BWilliams07 <104173554+BWilliams07@users.noreply.github.com>
2022-09-21 23:24:37 -0400

    Changes

--- src/main/java/org/launchcode/techjobs/mvc/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/HomeController.java
@@ -3,13 +3,9 @@ package org.launchcode.techjobs.mvc.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.HashMap;
 
-/**
- * Created by LaunchCode
- */
 @Controller
 public class HomeController {
 
